65acdb0a64a21839800ad475e2b4c90d53ef312e
jbradley84 <utbradley84@yahoo.com>
2022-05-30 12:58:53 -0500

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,9 +80,11 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
     $(event.target).addClass("dropover-active");
@@ -131,10 +133,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -157,7 +162,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
